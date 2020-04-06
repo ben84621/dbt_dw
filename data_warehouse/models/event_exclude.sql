dcce2f5b45c77968d8f2cf7df83adf0263062810
bfischer <bfischer@REM-VP-BF-ML.local>
2020-04-05 21:38:33 -0600

    updated to be sequential

--- data_warehouse/models/event_exclude.sql
+++ data_warehouse/models/event_exclude.sql
@@ -4,27 +4,17 @@
         unique_key='event_unique_id'
     )
 }}
--- 
--- select * from {{ ref('stg_event_exclude_far') }}
--- 
--- union 
--- 
--- select * from {{ ref('stg_event_exclude_dnu') }}
--- 
--- union 
--- 
--- select * from {{ ref('stg_event_exclude_test') }}
--- 
--- union 
 
---select * from {{ ref('stg_event_exclude_base') }}
+select * from {{ ref('stg_event_exclude_far') }}
 
-select event_unique_id
-	, context_id
-	, 'bfischer-dbt' as added_by
-	, 'Far future events' as exclude_reason
-	, current_date as insert_date
-	, true as is_valid
-from ticketing.events 
-where event_datetime > current_date + 7300
-and not is_exclude 
+union 
+ 
+select * from {{ ref('stg_event_exclude_dnu') }}
+
+union 
+ 
+select * from {{ ref('stg_event_exclude_test') }}
+ 
+union 
+
+select * from {{ ref('stg_event_exclude_base') }}
